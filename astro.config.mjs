b05b6844a14e7d83009a639f6f12cc85022bc5f5
noahzm <2554594+noahzm@users.noreply.github.com>
2026-07-09 14:35:19 -0400

    Add sitemap, robots.txt, prefetch, and image priority

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import icon from "astro-icon"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nm.works",
+  prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 })
